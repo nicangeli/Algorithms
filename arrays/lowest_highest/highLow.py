9fa0189f3582152f979201ec2c46a4c92cff67ea
Nicholas Angeli <nicangeli@gmail.com>
2014-03-02 21:46:29 +0000

    wrapped in class

--- arrays/lowest_highest/highLow.py
+++ arrays/lowest_highest/highLow.py
@@ -1,51 +1,57 @@
-def find_high_low(array, target):
-    low = find_low_index(array, target)
-    high = find_high_index(array, target)
+class HighLow:
 
-    return [low, high]
+    def find_high_low(self, array, target):
+        low = self.find_low_index(array, target)
+        high = self.find_high_index(array, target)
 
-def find_low_index(array, target):
-    low = 0
-    high = len(array)-1
+        return [low, high]
 
-    while low <= high:
-        mid = low + ((high-low)/2)
+    def find_low_index(self, array, target):
+        low = 0
+        high = len(array)-1
 
-        if array[mid] < target:
-            low = mid +1
-        elif array[mid] >= target:
-            high = mid -1
+        while low <= high:
+            mid = low + ((high-low)/2)
 
-    if low >= len(array):
-        return -1 
-    elif array[low] == target:
-        return low
-    else:
-        return -1
+            if array[mid] < target:
+                low = mid +1
+            elif array[mid] >= target:
+                high = mid -1
 
-def find_high_index(array, target):
-    low = 0
-    high = len(array)-1
-
-    while low <= high:
-        mid = low + ((high - low) / 2)
-
-        if array[mid] <= target:
-            low = mid + 1
+        if low >= len(array):
+            return -1 
+        elif array[low] == target:
+            return low
         else:
-            high = mid -1
+            return -1
 
-    if high >= len(array):
-        return -1
-    elif array[high] == target:
-        return high
-    else: 
-        return -1
+    def find_high_index(self, array, target):
+        low = 0
+        high = len(array)-1
+
+        while low <= high:
+            mid = low + ((high - low) / 2)
+
+            if array[mid] <= target:
+                low = mid + 1
+            else:
+                high = mid -1
+
+        if high >= len(array):
+            return -1
+        elif array[high] == target:
+            return high
+        else: 
+            return -1
 
 
 
 #Please learn how to do proper BDD (like rspec) with Python. Now. 
 if __name__ == "__main__":
+
+    highLow = HighLow()
+    array = [1, 2, 2, 5, 5, 5, 5, 5, 5, 11];
+    print highLow.find_high_low(array, 5)
 
     """
     a = [1, 2, 5, 5, 5, 7, 8, 9, 10, 11]
@@ -74,6 +80,3 @@ if __name__ == "__main__":
     a = [1, 2, 3, 4, 5, 5]
     print find_low_index(a, 6)
     """
-
-    array = [1, 2, 2, 5, 5, 5, 5, 5, 5, 11];
-    print find_high_low(array, 5)
